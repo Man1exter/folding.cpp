cbfc723ae572ba580f8f650e2e938a358f92a8ac
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-27 11:38:48 +0100

    left/right folding

--- fold.cpp
+++ fold.cpp
@@ -31,8 +31,13 @@ auto fun1(Args ... args){
 
 // lewy folding i prawy folding..
 template<typename ... Args>
-auto function2(Args ... args){
-    return (args - ...)
+auto fun2(Args ... args){
+    return (args - ...);
+}
+
+template<typename ... Args>
+auto fun3(Args ... args){
+    return (... - args);
 }
 
 int main(){
@@ -43,5 +48,13 @@ int main(){
     cout << fun1(1) << endl;
     cout << fun1() << endl;
 
+    cout << endl;
+
+    cout << fun2(100,50,20) << endl;  // 100 - (50 - 20) = wynik
+    cout << fun2(150,90,50) << endl;  // 150 - (90 - 50) = wynik
+
+    cout << fun3(170,45,32) << endl;  // (170 - 45) - 32 = wynik
+    cout << fun3(90,42,32) << endl;   // (90 - 42) - 32 = wynik
+ 
     return 0;
 }
